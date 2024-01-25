f7c6a6c60378dece805275988d5454690ea906ee
PankovskiyIM <63363659+DonRobot-all@users.noreply.github.com>
2024-01-25 12:29:02 +0300

    Add files via upload

---
+++ Scramblies.py
@@ -0,0 +1,39 @@
+"""
+https://www.codewars.com/kata/55c04b4cc56a697bb0000048
+Complete the function scramble(str1, str2) that returns true if a portion of str1 characters can be rearranged to match str2, otherwise returns false.
+
+Notes:
+
+Only lower case letters will be used (a-z). No punctuation or digits will be included.
+Performance needs to be considered.
+Examples
+scramble('rkqodlw', 'world') ==> True
+scramble('cedewaraaossoqqyt', 'codewars') ==> True
+scramble('katas', 'steak') ==> False
+
+"""
+
+
+# def scramble(string, word):
+#     word_dict = dict()
+#     for char in word:
+#         if char in word_dict:
+#             word_dict[char] = word_dict[char] + 1
+#         else:
+#             word_dict[char] = 1
+#     for char, count in word_dict.items():
+#         if not string.count(char) >= count:
+#             return False
+#     return True
+
+def scramble(string, word):
+    for char in set(word):
+        if string.count(char) < word.count(char):
+            return False
+    return True
+
+if __name__ == "__main__":
+    print(scramble('rkqodlw', 'world'))
+    print(scramble('cedewaraaossoqqyt', 'codewars'))
+    print(scramble('katas', 'steak'))
+
