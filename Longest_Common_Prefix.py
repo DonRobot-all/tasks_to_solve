ad4edb386263fc8370382d83a1eadb3d68dc4835
Ilya <pankovskiy.im@gmail.com>
2024-04-28 23:30:30 +0300

    genius desigion

--- Longest_Common_Prefix.py
+++ Longest_Common_Prefix.py
@@ -23,21 +23,30 @@ class Solution(object):
         :type strs: List[str]
         :rtype: str
         """
-        if len(strs) == 1:
-            return strs[0]
-        lenght_min = len(strs)
-        lenght_word = min([len(i) for i in strs])
-        if lenght_word == 0:
-            return ""
-        for idx in range(lenght_word):
-            for i in range(lenght_min):
-                if strs[i][idx] != strs[0][idx]:
-                    return strs[0][0:idx]
-        return strs[0][0:idx+1]
+        # if len(strs) == 1:
+        #     return strs[0]
+        # lenght_min = len(strs)
+        # lenght_word = min([len(i) for i in strs])
+        # if lenght_word == 0:
+        #     return ""
+        # for idx in range(lenght_word):
+        #     for i in range(lenght_min):
+        #         if strs[i][idx] != strs[0][idx]:
+        #             return strs[0][0:idx]
+        # return strs[0][0:idx+1]
+        l = list(zip(*strs))
+        result = ''
+        for i in l:
+            if len(set(i)) == 1:
+                result += i[0]
+            else:
+                break
+        return result
+
         
 
 example = Solution()
-assert example.longestCommonPrefix(["flower","flow","flight"]) == "fl"
+assert example.longestCommonPrefix(["flower","flow","flight"]) == "fl", print(example.longestCommonPrefix(["flower","flow","flight"]))
 assert example.longestCommonPrefix(["dog","racecar","car"]) == ""
 assert example.longestCommonPrefix([""]) == ""
 assert example.longestCommonPrefix(["", ""]) == ""
